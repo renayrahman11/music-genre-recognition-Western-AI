34d0c7abe9857a3879c87377c759b1c61f230ea1
renayrahman11 <74384891+renayrahman11@users.noreply.github.com>
2020-12-07 18:22:23 -0500

    update

--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ import librosa
 import pickle
 from sklearn import preprocessing
 import python_speech_features as mfcc
-from keras.models import load_model
+from tensorflow.python.keras.models import Model, load_model
 from tensorflow.compat.v1.keras.backend import set_session
 import matplotlib.pyplot as plt
 import tensorflow as tf
@@ -14,12 +14,10 @@ from werkzeug.utils import secure_filename
 
 print ("loading model")
 
-global set_session
-sess = tf.compat.v1.Session()
+
 global model
 model = load_model('Model2.h5')
-global graph
-graph = tf.compat.v1.get_default_graph()
+
 
 UPLOAD_FOLDER = "/Users/renayrahman/Documents/GitHub/music-genre-recognition-Western-AI/app/static"
 
@@ -76,34 +74,28 @@ def main_page():
         file = request.files['file']
         filename = secure_filename(file.filename)
         file.save(os.path.join('app/uploads', filename))
-        return redirect(url_for('/prediction/<filename>', filename=filename))
+        return redirect(url_for('prediction', filename=filename))
     return render_template('public/index.html')
 
 @app.route('/prediction/<filename>')
-def extract_features():
-    signal, sr = librosa.load ("uploads/<filename>")
-    mfccs = librosa.feature.mfcc(signal,n_mfccs=13,sr=sr)
-    return mfccs
 
 def prediction(filename):
     #Step 1
-    song_features = extract_features()
-    
+    song_features = np.array([[ 3.49943221e-01,  1.78442045e+03,  2.00265019e+03,  3.80648532e+03,
+        8.30663910e-02, -1.13596748e+02,  1.21557297e+02, -1.91588268e+01,
+        4.23510284e+01, -6.37645817e+00,  1.86188755e+01, -1.36979122e+01,
+        1.53446312e+01, -1.22852669e+01,  1.09804916e+01, -8.32432461e+00,
+        8.81066894e+00, -3.66736817e+00,  5.75169086e+00, -5.16276264e+00,
+        7.50947773e-01, -1.69193780e+00, -4.09952581e-01, -2.30020881e+00,
+        1.21992850e+00]])
+    print(song_features.shape)
     #Step 2
-    with graph.as_default():
-      set_session(sess)
-      prediction = model.predict_classes(song_features)
-      print(prediction)
+    
+    prediction = model.predict_classes(song_features)
+    print(prediction)
 #Step 4
-      number_to_class = ['blues', 'classical', 'country', 'disco', 'hiphop', 'jazz', 'metal', 'pop', 'reggae', 'rock']
-      index = np.argsort(prediction)
-      predictions = {
-        "class1":number_to_class[index[9]],
-        "class2":number_to_class[index[8]],
-        "class3":number_to_class[index[7]],
-        "prob1":prediction[index[9]],
-        "prob2":prediction[index[8]],
-        "prob3":prediction[index[7]],
-      }
+    number_to_class = ['blues', 'classical', 'country', 'disco', 'hiphop', 'jazz', 'metal', 'pop', 'reggae', 'rock']
+    index = np.argmax(prediction)
+    predicted_class = number_to_class[index]
 #Step 5
-    return render_template('predict.html', predictions=predictions)
+    return render_template('public/predict.html', predictions=predicted_class)
